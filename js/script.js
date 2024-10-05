90616f8637d26a69e95d1ec3a026355f8ca3b185
Goldengamer567 <140253261+Ammar2012hero@users.noreply.github.com>
2024-10-05 20:20:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**========= typing animation */
+
+var typed = new Typed(".typing", {
+    strings:["","Mobile Application Developer","Web Developer","Instructor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
